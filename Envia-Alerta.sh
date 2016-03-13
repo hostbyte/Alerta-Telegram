6a769a494db6333ffbb7313a7347957475121689
Alessandro Almeida <alessandro@isphost.com.br>
2016-03-12 21:29:17 -0300

    Create Envia-Alerta.sh

---
+++ Envia-Alerta.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## Substituir 'to' com o nome da conta ou grupo
+
+to=Usuário ou Grupo que vai receber os alertas
+
+##
+
+function show_usage {
+
+        echo "Usage $0 [message]"
+        exit
+}
+
+
+
+
+if [ $# -lt 1 ]
+then
+  show_usage
+fi
+
+(echo "contact_list";sleep 10;echo "msg $to $1"; echo "contact_list";sleep 10; echo "send_text $to /root/Alerta.txt"; echo "safe_quit") | Telegram
