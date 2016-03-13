62cd314c69b50fe1621bca978f4e73eb5a65c65d
Alessandro Almeida <alessandro@isphost.com.br>
2016-03-13 15:39:45 -0300

    Update Envia-Alerta.sh

--- Envia-Alerta.sh
+++ Envia-Alerta.sh
@@ -4,10 +4,13 @@
 # Telefone: (53) 8125-9194
 # Website: https://www.isphost.com.br
 
-# Substituir 'to' com o nome da conta ou grupo
+# Substituir 'user1= e user2=' com o nome da conta que vai receber os alertas.
+# Você também pode adicionar mais usuários, basta inserir abaixo:
+# Exemplo: user3=fulano e assim por diante.
+# Não esqueça de adiciona-los no final do codigo.
 
-to=Usuário ou Grupo que vai receber os alertas
-
+user1=Alessandro
+user2=Gabriel
 
 function show_usage {
 
@@ -23,4 +26,4 @@ then
   show_usage
 fi
 
-(echo "contact_list";sleep 10;echo "msg $to $1"; echo "contact_list";sleep 10; echo "send_text $to Alerta.txt"; echo "safe_quit") | Telegram
+(echo "contact_list"; sleep 10; echo "msg $user1 $1"; echo "msg $user2 $1"; echo "contact_list"; sleep 10; echo "send_text $user1 /root/Alerta.txt"; echo "send_text $user2 /root/Alerta.txt"; echo "safe_quit") | Telegram
